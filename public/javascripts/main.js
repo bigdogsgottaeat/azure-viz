6cd4a44b2c9afa5a6a583751718d507928069f9b
thesourorange <Neil.Brittliff@homemail.com.au>
2017-09-29 15:50:22 +1000

    Added AJAX calls

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    $('#criteria').on('keyup', function(e) {
+        var parameters = {filter:'all'};
+
+        if (e.keyCode == 13) {
+            $.get('/retrieve', parameters, function(data) {
+                alert('fred');
+            });
+        }        
+
+    });
+   
+});
